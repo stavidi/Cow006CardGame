e1c0c06ff910287674d7639d193f5dcf32a0f9f6
Tatyana Derbysheva <tatyderb@gmail.com>
2018-04-03 12:29:48 +0300

    test room 701

--- witch.py
+++ witch.py
@@ -1,6 +1,6 @@
 from witch_game import WitchGame
 from witch_player import WitchHuman, WitchRandomComputer
-
+"""MAIN FILE"""
 if __name__ == "__main__":
   WitchGame([
     WitchRandomComputer("Alice"),
